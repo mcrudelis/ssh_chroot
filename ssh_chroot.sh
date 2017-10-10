a1987531e4bd627110c4dc83545605bbef9c6a55
Maniack Crudelis <maniackc_dev@crudelis.fr>
2017-10-11 01:33:51 +0200

    Fix

--- ssh_chroot.sh
+++ ssh_chroot.sh
@@ -4,15 +4,15 @@
 ssh_chroot_set_directory () {
 	local chroot_dir="$1"
 	# Create binaries directories
-	sudo mkdir -p $chroot_dir/{bin,lib,lib64}
+	sudo mkdir -p $chroot_dir/{bin,lib,lib64,data}
 
 	# Copy the ld-linux file, according to the architecture
 	copy_ld-linux () {
-		test -e "$1" && sudo cp "$1" $chroot_dir/lib/
+		test -e "$1" && sudo cp "$1" $chroot_dir/$2/
 	}
-	copy_ld-linux /lib/ld-linux.so.2
-	copy_ld-linux /lib64/ld-linux-x86-64.so.2
-	copy_ld-linux /lib/ld-linux-armhf.so.3
+	copy_ld-linux /lib/ld-linux.so.2 lib
+	copy_ld-linux /lib64/ld-linux-x86-64.so.2 lib64
+	copy_ld-linux /lib/ld-linux-armhf.so.3 lib
 }
 
 # Copy binary and its libraries into the chroot.
@@ -20,7 +20,7 @@ ssh_chroot_copy_binary () {
 	local chroot_dir="$2"
 	echo "Add the binary $1 in the chroot directory"
 	# Find and copy the binary file
-	sudo cp `which $1` $chroot_dir/bin/$1
+	sudo cp `which $1` $chroot_dir/bin/$(basename $1)
 	# Then search for its libraries
 	while read lib_file
 	do
@@ -34,15 +34,31 @@ ssh_chroot_copy_binary () {
 	done <<< "$(ldd `which $1`)"
 }
 
+# Copy some usual binaries in the chroot
+ssh_chroot_standard_binaries () {
+	ssh_chroot_copy_binary bash "$chroot_dir"
+	ssh_chroot_copy_binary cat "$chroot_dir"
+	ssh_chroot_copy_binary cp "$chroot_dir"
+	ssh_chroot_copy_binary du "$chroot_dir"
+	ssh_chroot_copy_binary ls "$chroot_dir"
+	ssh_chroot_copy_binary mkdir "$chroot_dir"
+	ssh_chroot_copy_binary mv "$chroot_dir"
+	ssh_chroot_copy_binary rm "$chroot_dir"
+	ssh_chroot_copy_binary rmdir "$chroot_dir"
+	ssh_chroot_copy_binary sftp "$chroot_dir"
+	ssh_chroot_copy_binary /usr/lib/openssh/sftp-server "$chroot_dir"
+}
+
+
 # Set permissions
 ssh_chroot_set_permissions () {
 	local chroot_dir="$1"
 	local user="$2"
 
-	sudo chown $user: -R "$chroot_dir"
-	sudo chown root: -R "$chroot_dir/"{bin,lib,lib64}
+	sudo chown root: -R "$chroot_dir"
+	sudo chown $user: -R "$chroot_dir/data"
 	# The parent directory shall be handled by root. It's necessary for chroot.
-	sudo chown root: "$chroot_dir"
+# 	sudo chown root: "$chroot_dir"
 }
 
 # Set the chroot in the ssh config for this user
@@ -50,11 +66,11 @@ ssh_chroot_add_chroot_config () {
 	local chroot_dir="$1"
 	local user="$2"
 
-	echo "
-	Match User $user # Automatically added for the user $user
-	ChrootDirectory $chroot_dir # Automatically added for the user $user
-	AllowTcpForwarding no # Automatically added for the user $user
-	X11Forwarding no # Automatically added for the user $user" | sudo tee -a /etc/ssh/sshd_config
+	echo -e "
+	Match User $user\t# Automatically added for the user $user
+	ChrootDirectory $chroot_dir\t# Automatically added for the user $user
+	AllowTcpForwarding no\t# Automatically added for the user $user
+	X11Forwarding no\t# Automatically added for the user $user" | sudo tee -a /etc/ssh/sshd_config
 
 	# Reload ssh service
 	sudo systemctl reload ssh
